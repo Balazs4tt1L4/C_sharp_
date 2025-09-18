4fd432063e689d8a848b7aa2c30782e779ef54e8
Balázs Attila <balazsa@KKSZKI.LOCAL>
2025-09-18 09:19:03 +0200

    feladat3 is ready.

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -38,6 +38,29 @@ namespace csharp
             {
                 Console.WriteLine("A szám nulla.");
             }
+
+            Console.WriteLine("Adj meg egy hónap számát [1-12]: ");
+            int honap = int.Parse(Console.ReadLine());
+            if (honap >= 3 && honap <= 5)
+            {
+                Console.WriteLine("Tavasz van.");
+            }
+            else if (honap >= 6 && honap <= 8)
+            {
+                Console.WriteLine("Nyár van.");
+            }
+            else if (honap >= 9 && honap <= 11)
+            {
+                Console.WriteLine("Ősz van.");
+            }
+            else if (honap == 12 || honap == 1 || honap == 2)
+            {
+                Console.WriteLine("Tél van.");
+            }
+            else
+            {
+                Console.WriteLine("Hibás hónap szám.");
+            }
             {
                 
             }
